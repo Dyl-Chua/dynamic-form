06da7bb0b345eddc0eb251cf977f39d9fdea5e48
Dyl-Chua <88299891+Dyl-Chua@users.noreply.github.com>
2021-11-20 08:49:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ var add_more_fields = document.getElementById('add_more_fields');
 var remove_fields = document.getElementById('remove_fields');
 var item_field = document.getElementById('item_field');
 
+
 add_more_fields.onclick = function(){
-    var newField = document.createElement('item_field');
+    var newField = document.createElement('input');
     newField.setAttribute('type','text');
-    newField.setAttribute('name','gobeli_orders[]');
-    newField.setAttribute('class','gobeli_orders');
+    newField.setAttribute('name','survey_options[]');
+    newField.setAttribute('class','survey_options');
     newField.setAttribute('siz',50);
     newField.setAttribute('placeholder','Another Field');
     survey_options.appendChild(newField);
 }
 
 remove_fields.onclick = function(){
-    var item_field_tags = survey_options.getElementsByTagName('item_field');
-    if(item_field_tags.length > 2) {
-        survey_options.removeChild(item_field_tags[(item_field_tags.length) - 1]);
+    var input_tags = survey_options.getElementsByTagName('input');
+    if(input_tags.length > 2) {
+        survey_options.removeChild(input_tags[(input_tags.length) - 1]);
     }
 }
